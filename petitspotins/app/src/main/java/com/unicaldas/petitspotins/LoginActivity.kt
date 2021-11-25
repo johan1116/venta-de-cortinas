7035f68f2bbf63ab646f6d386846aa34aafbb32f
Johan <johan23zulu@gmail.com>
2021-11-25 18:59:19 -0500

    se agregaron los fragmentos

---
+++ petitspotins/app/src/main/java/com/unicaldas/petitspotins/LoginActivity.kt
@@ -0,0 +1,61 @@
+package com.unicaldas.petitspotins
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+
+class LoginActivity : AppCompatActivity() {
+    private var edtUsername : EditText? = null
+    private var edtPassword : EditText? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        edtUsername = findViewById(R.id.edtUsername)
+        edtPassword = findViewById(R.id.edtPassword)
+
+
+    }
+
+
+
+
+
+
+
+
+    fun onLogin(botonLogin: View) {
+
+
+        var username:String = edtUsername!!.text.toString()
+        var password:String = edtPassword!!.text.toString()
+
+        if (username == "johan@gmail.com" && password == "1234"){
+
+
+
+            //TERCERA FORMA DE INGRESO CON UN DIALOGO EMERGENTE PERO QUE NO QUITA FUNCIONALIDAD
+
+            val intento = Intent(this,WelcomeActivity::class.java)
+            startActivity(intento)
+
+            Toast.makeText(applicationContext,getResources().getString(R.string.txt_welcome)+"!",
+                Toast.LENGTH_LONG).show()
+
+
+
+        }
+        else{
+
+
+
+            //SEGUNDA FORMA DE INVALIDAR EL USUARIO
+            Toast.makeText(this,getResources().getString(R.string.txt_error_inicio), Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
--- petitspotins/app/src/main/java/com/unicaldas/petitspotins/MainActivity.kt
+++ petitspotins/app/src/main/java/com/unicaldas/petitspotins/MainActivity.kt
@@ -4,77 +4,43 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
 
-    private var edtUsername : EditText? = null
-    private var edtPassword : EditText? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        edtUsername = findViewById(R.id.edtUsername)
-        edtPassword = findViewById(R.id.edtPassword)
+        val inicio = findViewById<ImageView>(R.id.inicio)
+
+        inicio.setOnClickListener(clickListener)
+
 
 
     }
 
-    fun onLogin(botonLogin: View) {
+    val clickListener = View.OnClickListener {view ->
 
+        when (view.getId()) {
+            R.id.inicio-> irlogin()
 
-        var username:String = edtUsername!!.text.toString()
-        var password:String = edtPassword!!.text.toString()
-
-        if (username == "johan@gmail.com" && password == "1234"){
-
-            //PRIMERA FORMA DE INGRESO
-
-            /*val intento = Intent(this,WelcomeActivity::class.java)
-            startActivity(intento)*/
-
-            //SEGUNDA FORMA DE INGRESO MEDIANTE DIALOGOS EMERGENTES
-            /*
-            val positiveButton = {dialog:DialogInterface,which:Int ->
-                val intento = Intent(this,WelcomeActivity::class.java)
-                startActivity(intento)
-
-            }
-
-            val negativeButton = {_:DialogInterface,_:Int ->}
-
-            val dialog = AlertDialog.Builder(this)
-                .setTitle(getResources().getString(R.string.txt_welcome)+"!")
-                .setMessage(getResources().getString(R.string.txt_user)+edtUsername!!.text.toString())
-                .setPositiveButton(getResources().getString(R.string.txt_ok),positiveButton)
-                .setNegativeButton(getResources().getString(R.string.txt_cancel),negativeButton)
-
-            dialog.create()
-            dialog.show()*/
-
-            //TERCERA FORMA DE INGRESO CON UN DIALOGO EMERGENTE PERO QUE NO QUITA FUNCIONALIDAD
-
-            val intento = Intent(this,AbonoActivity::class.java)
-            startActivity(intento)
-
-            Toast.makeText(applicationContext,getResources().getString(R.string.txt_welcome)+"!",
-                Toast.LENGTH_LONG).show()
 
 
 
         }
-        else{
-
-            //PRIMERA FORMA DE INVALIDAR EL USUARIO
-            /*
-            val dialog = AlertDialog.Builder(this).setTitle(getResources().getString(R.string.txt_error))
-                .setMessage(getResources().getString(R.string.txt_error_inicio)).create().show()*/
-
-            //SEGUNDA FORMA DE INVALIDAR EL USUARIO
-            Toast.makeText(this,getResources().getString(R.string.txt_error_inicio), Toast.LENGTH_SHORT).show()
-        }
-
     }
+
+    private fun irlogin() {
+        val intento = Intent(this, LoginActivity::class.java)
+        startActivity(intento)
+    }
+
+
 }
